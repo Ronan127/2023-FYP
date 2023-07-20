78b3cd66c1b2e9fcf57c226ef0e6fe41bc41f197
Ronan <wellsronan@gmail.com>
2023-07-20 21:18:41 +0200

    fix file references

--- vscode/sdf-formatter.py
+++ vscode/sdf-formatter.py
@@ -7,7 +7,7 @@ data = data.replace(".stl", ".obj")
 #remove frames (drake doesn't like them)
 data = re.sub("((<frame\sname=).*(<\/frame>\n))|(( frame=)[^>]*)", "", data)
 #fix file referencing
-data = re.sub("(file://)(?!my-robot/)", "file://my-robot/", data)
+data = re.sub("((file://)(?!my-robot/)|(file://my-robot/))", "", data)
 
 sdfile = open("./my-robot/robot.sdf", "w")
 sdfile.write(data)
