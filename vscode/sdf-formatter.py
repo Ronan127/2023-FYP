30a0232248e08ed3593b4e73207d76b81904a18e
Ronan <wellsronan@gmail.com>
2023-07-20 20:52:14 +0200

    fix file paths, delete examples

--- vscode/sdf-formatter.py
+++ vscode/sdf-formatter.py
@@ -2,8 +2,12 @@ import re
 
 sdfile = open("./my-robot/robot.sdf", "r")
 data=sdfile.read()
+#replace references to stl files with references to obj files
 data = data.replace(".stl", ".obj")
+#remove frames (drake doesn't like them)
 data = re.sub("((<frame\sname=).*(<\/frame>\n))|(( frame=)[^>]*)", "", data)
+#fix file referencing
+data = re.sub("(file://)(?!my-robot/)", "file://my-robot/", data)
 
 sdfile = open("./my-robot/robot.sdf", "w")
 sdfile.write(data)
