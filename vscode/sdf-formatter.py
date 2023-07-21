ff880ad889f15e53e47b1c251d3edfab8b0353f5
Ronan <wellsronan@gmail.com>
2023-07-21 16:02:34 +0200

    re add 2wheels

--- vscode/sdf-formatter.py
+++ vscode/sdf-formatter.py
@@ -1,13 +1,21 @@
 import re
+import argparse
+import pathlib
 
-sdfile = open("./my-robot/robot.sdf", "r")
-data=sdfile.read()
-#replace references to stl files with references to obj files
-data = data.replace(".stl", ".obj")
-#remove frames (drake doesn't like them)
-data = re.sub("((<frame\sname=).*(<\/frame>\n))|(( frame=)[^>]*)", "", data)
-#fix file referencing
-data = re.sub("((file://)(?!my-robot/)|(file://my-robot/))", "", data)
+parser = argparse.ArgumentParser("format sdformat for drake")
+parser.add_argument("root", type=str, help="The root directory that contains sdf file")
+args = parser.parse_args()
 
-sdfile = open("./my-robot/robot.sdf", "w")
-sdfile.write(data)
+for file in pathlib.Path(args.root).glob('*.sdf'):
+    print(f"Formatting {file}")
+    sdfile = open(file, "r")
+    data=sdfile.read()
+    #replace references to stl files with references to obj files
+    data = data.replace(".stl", ".obj")
+    #remove frames (drake doesn't like them)
+    data = re.sub("((<frame\sname=).*(<\/frame>\n))|(( frame=)[^>]*)", "", data)
+    #fix file referencing
+    data = re.sub("((file://)(?!{})|(file://{}))".format(args.root, args.root), "", data)
+
+    sdfile = open(file, "w")
+    sdfile.write(data)
