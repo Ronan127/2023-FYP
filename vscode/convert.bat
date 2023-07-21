6c028fadfd28be399984e8dc78217e06649c032d
Ronan <wellsronan@gmail.com>
2023-07-21 17:15:41 +0200

    started flattenning

--- vscode/convert.bat
+++ vscode/convert.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set ONSHAPE_API="https://cad.onshape.com"
-set ONSHAPE_ACCESS_KEY="dvzjop0q6z8LowpPDrYt7ZGT"
-set ONSHAPE_SECRET_KEY="cpHZ3UTDp2Z8GrIPbPJYDb0o0Il6YFDCswr3i7btPvoi5CWi"
+set ONSHAPE_API=https://cad.onshape.com
+set ONSHAPE_ACCESS_KEY=dvzjop0q6z8LowpPDrYt7ZGT
+set ONSHAPE_SECRET_KEY=cpHZ3UTDp2Z8GrIPbPJYDb0o0Il6YFDCswr3i7btPvoi5CWi
 
 onshape-to-robot %1
 python sdf-formatter.py %1
