20429db1956aff7021717fb73edaaefb399899e0
Ronan Wells <wellsronan@gmail.com>
2023-10-27 08:45:44 +0200

    preparing simulation

--- vscode/convert.sh
+++ vscode/convert.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#Obtained at https://dev-portal.onshape.com/keys
 export ONSHAPE_API=https://cad.onshape.com
 export ONSHAPE_ACCESS_KEY=dvzjop0q6z8LowpPDrYt7ZGT
 export ONSHAPE_SECRET_KEY=cpHZ3UTDp2Z8GrIPbPJYDb0o0Il6YFDCswr3i7btPvoi5CWi
@@ -7,5 +8,5 @@ export ONSHAPE_SECRET_KEY=cpHZ3UTDp2Z8GrIPbPJYDb0o0Il6YFDCswr3i7btPvoi5CWi
 onshape-to-robot $1
 python sdf-formatter.py $1
 python stl2obj.py $1 --extension stl
-python set_friction.py $1 --wheel 1.15 --tail 0.1
+python set_friction.py $1 --wheel 1.15 --tail 0.1 --frame 0.1
 
